39e201e92af7ad50fb6d4aa6d413c01cfea7246a
bhargav157 <bhargavgupta001@gmail.com>
2024-04-11 16:15:39 +0530

    discord_frontend

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        deepBlue: "#404eed",
+      }
+    },
+  },
+  plugins: [],
+  fontFamily:{
+    Whitney:["Whitney"],
+    Ginto:["Ginto"],
+    ggSans:["ggSans"],
+  },
+}
+
